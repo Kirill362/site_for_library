f13f26d28e410fe352538d62304b5a0434e0cc4e
Kirill362 <64096313+Kirill362@users.noreply.github.com>
2020-06-18 19:41:24 +0300

    wip

--- render_website.py
+++ render_website.py
@@ -3,6 +3,7 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 import os
 import more_itertools
+import math
 
 
 def on_reload():
@@ -12,7 +13,9 @@ def on_reload():
             autoescape=select_autoescape(['html', 'xml'])
         )
         template = env.get_template('template.html')
-        rendered_page = template.render(books=page_books,)
+        rendered_page = template.render(books=page_books,
+                                        pages_amount=math.ceil(len(books)/20),
+                                        page_id=page_id,)
         with open(f'pages/index{page_id}.html', 'w', encoding="utf8") as file:
             file.write(rendered_page)
 
@@ -26,6 +29,6 @@ chunked_books = more_itertools.chunked(books, 20)
 
 on_reload()
 server = Server()
-server.watch('template.html', on_reload)
+server.watch('template.html', on_reload())
 server.serve(root='.')
 
